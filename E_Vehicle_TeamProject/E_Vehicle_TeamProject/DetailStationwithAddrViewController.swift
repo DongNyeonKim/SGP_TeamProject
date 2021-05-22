9519ad1a3c6875935417c2dac65fd525bfa4801e
KDY9687 <jungsang3488@gmail.com>
2021-05-23 01:43:53 +0900

    all done parsing

---
+++ E_Vehicle_TeamProject/E_Vehicle_TeamProject/DetailStationwithAddrViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailStationwithAddrViewController.swift
+//  E_Vehicle_TeamProject
+//
+//  Created by kpugame on 2021/05/23.
+//
+
+import UIKit
+
+class DetailStationwithAddrViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
