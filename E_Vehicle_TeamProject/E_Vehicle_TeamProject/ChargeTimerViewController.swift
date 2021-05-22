0115ede97af3ad336af9f8b529776de5f081ad75
he0rin <her0in941018@gmail.com>
2021-05-22 20:42:46 +0900

    타이머 예상시간

--- E_Vehicle_TeamProject/E_Vehicle_TeamProject/ChargeTimerViewController.swift
+++ E_Vehicle_TeamProject/E_Vehicle_TeamProject/ChargeTimerViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 class ChargeTimerViewController: UIViewController {
     
     @IBOutlet weak var pickerView: UIPickerView!
-
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var predictingTime: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    var activeTimer: Bool = false
+    var timer = Timer()
+    
     var hourPickerDataSource = [0...24]
     var minPickerDataSource = [0...60]
     var secPickerDataSource = [0...60]
@@ -19,20 +25,90 @@ class ChargeTimerViewController: UIViewController {
     var min:Int = 0
     var sec:Int = 0
     
+    var saveHour: Int = 0
+    var saveMin: Int = 0
+    var saveSec: Int = 0
+    
     @IBAction func StartButton() {
-        
+        if(!activeTimer){
+            activeTimer = true
+            pickerView.isHidden = true
+            timerLabel.isHidden = false
+            if(!timer.isValid) {
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.subtrackTime), userInfo: nil, repeats: true)
+            }
+            button.setTitle("리셋", for: .normal)
+            
+//            var formatter = DateFormatter()
+//            formatter.dateFormat = "HH:mm:ss"
+//            var current_date_string = formatter.string(from: Date())
+//            print(current_date_string)
+            
+            let date = Date()
+            let date1 = Calendar.current.date(byAdding: .second, value: hour*3600+min*60+sec, to: date)
+            var dateFormatter: DateFormatter = {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "YYYY-MM-dd\nHH:mm:ss"
+                
+                return dateFormatter
+            }()
+
+            print(date1)
+            print(dateFormatter.string(from: date1!))
+
+            predictingTime.text = "예상 충전 완료 시간\n\(dateFormatter.string(from: date1!))"
+        }
+        else {
+            activeTimer = false
+            setupTimer()
+        }
     }
-    @IBAction func StopButton() {
+    
+    func setupTimer() {
+        hour = 0
+        min = 0
+        sec = 0
         
+        var hstr = String(format: "%02d", hour)
+        var mstr = String(format: "%02d", min)
+        
+        timerLabel.text = "남은시간\n\(hstr):\(mstr):\(sec)"
+        timerLabel.isHidden = true
+        pickerView.isHidden = false
+        predictingTime.text = "             시간            분               초"
+        button.setTitle("시작", for: .normal)
     }
-    @IBAction func ResetButton() {
-        
+    
+    @objc func subtrackTime() {
+        if(!(hour == 0 && min == 0 && sec == 0)){
+            sec -= 1
+            
+            if(sec == -1){
+                sec = 59
+                min -= 1
+            }
+            
+            if(min == -1){
+                min = 59
+                hour -= 1
+            }
+            
+            var hstr = String(format: "%02d", hour)
+            var mstr = String(format: "%02d", min)
+            
+            timerLabel.text = "남은시간\n\(hstr):\(mstr):\(sec)"
+        }
+        else {
+            timer.invalidate()
+            timerLabel.text = "충전완료\n00:00:0"
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         pickerView.delegate = self
+        setupTimer()
         // Do any additional setup after loading the view.
     }
 }
