e727a9486b29617e5080b29bb602c8e49cf11911
KDY9687 <jungsang3488@gmail.com>
2021-06-07 03:03:01 +0900

    voice func add

--- E_Vehicle_TeamProject/E_Vehicle_TeamProject/ViewController_2.swift
+++ E_Vehicle_TeamProject/E_Vehicle_TeamProject/ViewController_2.swift
@@ -5,11 +5,112 @@
 //  Created by kpugame on 2021/05/21.
 //
 import UIKit
+import Speech
+
 
 class ViewController_2: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
  
     @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet var sidoVoiceStartBtn: UIButton!
+    @IBOutlet var sidoVoiceEndBtn: UIButton!
+    var sidoVoiceText: String!
+    
+    @IBOutlet var adrVoiceStartBtn: UIButton!
+    @IBOutlet var adrVoiceEndBtn: UIButton!
+    var adrVoiceText: String!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko-KR"))!
+    
+    private var speechRecognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var speechRecognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
+    
+    @IBAction func startTranscribing(_ sender: Any) {
+        sidoVoiceStartBtn.isEnabled = false
+        sidoVoiceEndBtn.isEnabled = true
+        try! startSession()
+
+    }
+    @IBAction func stopTranscribing(_ sender: Any) {
+            if audioEngine.isRunning {
+                audioEngine.stop()
+                speechRecognitionRequest?.endAudio()
+                sidoVoiceStartBtn.isEnabled = true
+                sidoVoiceEndBtn.isEnabled = false
+            }
+        print(self.sidoVoiceText)
+            switch (self.sidoVoiceText) {
+            case "서울특별시" : self.pickerView.selectRow(0, inComponent: 0, animated: true)
+                zcode = "11"
+                break
+            case "부산광역시" : self.pickerView.selectRow(1, inComponent: 0, animated: true)
+                zcode = "26"
+                break
+            case "대구광역시" : self.pickerView.selectRow(2, inComponent: 0, animated: true)
+                zcode = "27"
+                break
+            case "인천시" : self.pickerView.selectRow(3, inComponent: 0, animated: true)
+                zcode = "28"
+                break
+            case "광주시" : self.pickerView.selectRow(4, inComponent: 0, animated: true)
+                zcode = "29"
+                break
+            case "대전시" : self.pickerView.selectRow(5, inComponent: 0, animated: true)
+                zcode = "30"
+                break
+            case "울산시" : self.pickerView.selectRow(6, inComponent: 0, animated: true)
+                zcode = "31"
+                break
+            case "경기도" : self.pickerView.selectRow(7, inComponent: 0, animated: true)
+                zcode = "41"
+                break
+            case "강원도" : self.pickerView.selectRow(8, inComponent: 0, animated: true)
+                zcode = "42"
+                break
+            case "충청북도" : self.pickerView.selectRow(9, inComponent: 0, animated: true)
+                zcode = "43"
+                break
+            case "충청남도" : self.pickerView.selectRow(10, inComponent: 0, animated: true)
+                zcode = "44"
+                break
+            case "전라북도" : self.pickerView.selectRow(11, inComponent: 0, animated: true)
+                zcode = "45"
+                break
+            case "전라남도" : self.pickerView.selectRow(12, inComponent: 0, animated: true)
+                zcode = "46"
+                break
+            case "경상북도" : self.pickerView.selectRow(13, inComponent: 0, animated: true)
+                zcode = "47"
+                break
+            case "경상남도" : self.pickerView.selectRow(14, inComponent: 0, animated: true)
+                zcode = "48"
+                break
+            case "제주도" : self.pickerView.selectRow(15, inComponent: 0, animated: true)
+                zcode = "50"
+                break
+                
+            default: break
+            }
+    }
+    
+    @IBAction func startAdrTranscribing(_ sender: Any) {
+        adrVoiceStartBtn.isEnabled = false
+        adrVoiceEndBtn.isEnabled = true
+        try! startAdrSession()
+    }
+    
+    @IBAction func endAdrTranscribing(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            speechRecognitionRequest?.endAudio()
+            adrVoiceStartBtn.isEnabled = true
+            adrVoiceEndBtn.isEnabled = false
+        }
+    }
     
     @IBOutlet var textView: UITextField!
     
@@ -74,7 +175,7 @@ class ViewController_2: UIViewController, UIPickerViewDelegate, UIPickerViewData
         } else if row == 15 {
             zcode = "50"   //제주도
         } else {
-            zcode = "11"   //종로구
+            zcode = "11"   //서울특별시
         }
     }
     
@@ -98,8 +199,139 @@ class ViewController_2: UIViewController, UIPickerViewDelegate, UIPickerViewData
         }
     }
     
+    
+    //음성인식
+    func startSession() throws {
+        if let recognitionTask = speechRecognitionTask {
+            speechRecognitionTask?.cancel()
+            self.speechRecognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSession.Category.record)
+        
+        speechRecognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let recognitionRequest = speechRecognitionRequest else
+        { fatalError("SFSpeechAudioBufferRecognitionRequset object creation failed")}
+        
+        let inputNode = audioEngine.inputNode
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        speechRecognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            
+            var finished = false
+            
+            if let result = result {
+                self.sidoVoiceText =
+                    result.bestTranscription.formattedString
+                finished = result.isFinal
+            }
+            
+            if error != nil || finished {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.speechRecognitionRequest = nil
+                self.speechRecognitionTask = nil
+                
+                self.sidoVoiceStartBtn.isEnabled = true
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.speechRecognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        
+    }
+    
+    func startAdrSession() throws {
+        if let recognitionTask = speechRecognitionTask {
+            speechRecognitionTask?.cancel()
+            self.speechRecognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSession.Category.record)
+        
+        speechRecognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let recognitionRequest = speechRecognitionRequest else
+        { fatalError("SFSpeechAudioBufferRecognitionRequset object creation failed")}
+        
+        let inputNode = audioEngine.inputNode
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        speechRecognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            
+            var finished = false
+            
+            if let result = result {
+                self.textView.text =
+                    result.bestTranscription.formattedString
+                finished = result.isFinal
+            }
+            
+            if error != nil || finished {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.speechRecognitionRequest = nil
+                self.speechRecognitionTask = nil
+                
+                self.adrVoiceStartBtn.isEnabled = true
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.speechRecognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        
+    }
+    
+    
+    func authorizeSR() {
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.sidoVoiceStartBtn.isEnabled = true
+                    
+                case .denied:
+                    self.sidoVoiceStartBtn.isEnabled = false
+                    self.sidoVoiceStartBtn.setTitle("Speech recognition access denied by user", for: .disabled)
+                    
+                case .restricted:
+                    self.sidoVoiceStartBtn.isEnabled = false
+                    self.sidoVoiceStartBtn.setTitle("Speech recognition restricted on device", for: .disabled)
+                    
+                case .notDetermined:
+                    self.sidoVoiceStartBtn.isEnabled = false
+                    self.sidoVoiceStartBtn.setTitle("Speech recognition not authorized", for: .disabled)
+
+                }
+            }
+        }
+    }
+    
+    
 override func viewDidLoad() {
     super.viewDidLoad()
+    authorizeSR()
     
     self.pickerView.delegate = self;
     self.pickerView.dataSource = self;
